d732ad9ff155670001552b07f77b4cd62092cbf3
Yonkoulong <nguyenhailong270921@gmail.com>
2021-09-19 22:58:37 +0700

    19/09/2021

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 30000
-
-app.use(morgan('combined'))
-
-app.get('/tin-tuc', (req, res) => res.send('Hello Word!'))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
